3092d012ab854179cdb2ddf7b170657f6fd87b56
Philippe Paravicini <philippe@paravicini.com>
2012-11-07 17:57:01 -0800

    replace cradle with nano as couch access lib

--- ImageService/lib/plm-image/ImageService.js
+++ ImageService/lib/plm-image/ImageService.js
@@ -3,8 +3,9 @@ var  fs  = require('fs')
     ,gm  = require('gm')
     ,cs  = require('./checksum')
     ,Image  = require('./Image')
-    ,cradle = require('cradle')
+    ,nano = require('nano')
     ,Step   = require('step')
+    ,_    = require('underscore')
 ;
 
 var config = {
@@ -25,12 +26,39 @@ var checkConfig = function() {
   console.log('plm-image/ImageService: Config OK...');
 };
 
+// returns a db connection 
+var _db = function () {
+  return nano('http://' + config.db.host + ':' + config.db.port + '/' + config.db.name);
+}
+
+// checks to see whether an image with the given doc_id already exists,
+// and passes current version to callback; this is useful in some cases
+var findVersion = exports.findVersion = function (doc_id, callback) {
+  var db = _db();
+  Step(
+    function () {
+      console.log("getting version for doc_id: %j", doc_id);
+      db.head(doc_id, this);
+    },
+    function (err, body, hdr) {
+      if (err) {
+        if (err.scope == 'couch' && err.status_code == 404) { 
+        // there is no doc with that id, return null
+        callback(null); return;
+        } else { throw err;} // some other error
+      }
+      console.log("version is: %j", JSON.parse(hdr.etag));
+      callback(JSON.parse(hdr.etag));
+    }
+  );
+}
+
+
 exports.save = function(imagePath, callback) 
 {
   checkConfig();
-  var db = new(cradle.Connection)('http://' + config.db.host,
-                                  config.db.port).database(config.db.name),
-      image = new Image({path:imagePath});
+  var db = nano('http://' + config.db.host + ':' + config.db.port + '/' + config.db.name);
+  var image = new Image({path:imagePath});
 
   // console.log("new image: " + JSON.stringify(image,null,"  "));
 
@@ -56,29 +84,40 @@ exports.save = function(imagePath, callback)
       console.log("populating image");
       if (err) { callback(err); return; }
       image.readFromGraphicsMagick(data);
-      console.log("parsed image: " + JSON.stringify(image,null,"  "));
-      this(err, image);
+      // console.log("parsed image: " + JSON.stringify(image,null,"  "));
+      this(err, image.path);
     },
 
-    function (err, anImage) {
+    function (err, anImagePath) {
+      console.log("checking to see if file exists...");
+      if (err) { callback(err); return; }
+      findVersion(anImagePath, this);
+    },
+
+    function (version) {
       console.log("saving to db...");
-      if (err) { callback(err); return; }
-      db.save(anImage.path, anImage, this);
+      if (_.isString(version)) {
+        image["_rev"] = version;
+      }
+
+      db.insert(image,imagePath,this);
     },
 
-    function (err, result) {
-      console.log("saving attachment...");
+    function (err, body, headers) {
       if (err) { callback(err); return; }
-      console.log("result from 'save': " + JSON.stringify(result));
+      // console.log("result from 'save': " + JSON.stringify(headers));
+      console.log("result from 'save': " + JSON.stringify(body));
+      console.log("saving attachment...");
       
       fs.createReadStream(imagePath).pipe(
-        db.saveAttachment(
-          result.id, 
-          {
-            name: image.path, 
-            contentType: 'image/' + image.format, 
-          }, callback)
+        db.attachment.insert(
+          body.id, 
+          image.path,
+          null,
+          'image/'+image.format,
+          {rev: body.rev}, callback)
       );
     }
   );
 };
+
